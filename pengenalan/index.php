46a7f2200f787a04336e59655545bcf0fcef7f52
imbasri <imbasri.dev@gmail.com>
2025-01-10 09:48:02 +0700

    pengenalan

---
+++ pengenalan/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengenalan PHP</title>
+</head>
+
+<body>
+    <h1><?php echo 'Pengenalan PHP'; ?></h1>
+    <h1><?= 'Pengenalan PHP Tanpa Echo'; ?></h1>
+</body>
+
+</html>
+<?php
+echo 'hi world <br>';
+echo 'hai php 2025';
+?>
