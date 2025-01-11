49c39d83a6eb5fe2e4323920e2bb0408f6be6713
imbasri <imbasri.dev@gmail.com>
2025-01-11 15:16:28 +0700

    include-require

---
+++ include-require/index.php
@@ -0,0 +1,7 @@
+<?php 
+// require jika terjadi error maka script akan dihentikan
+// include jika terjadi error maka script akan terus berjalan ke bawah/script selanjutnya.
+echo "content <br>";
+require "footer1.php";
+echo "setelah content <br>";
+?>
